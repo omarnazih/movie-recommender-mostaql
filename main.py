33952dd7f71f251c395b4123a177646b4b6b53e0
omar nazih <omarnazeh295@gmail.com>
2023-05-14 21:33:19 +0000

    updates

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def get_content_recommendations(title):
     return jsonify({'data': recommendations, 'status': 200})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
